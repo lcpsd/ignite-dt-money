af3de2d80718eea0fdbbdc8ed7294a13e3974d12
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-11-04 14:17:06 -0300

    summary styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { GlobalStyle } from './styles/global';
 import {Header} from './components/Header'
+import { Dashboard } from './components/Dashboard';
 function App() {
   return (
     <>
       <Header/>
+      <Dashboard/>
       <GlobalStyle/>
     </>
   );
