17ce21143de6148ada7e85e6239a886a26b3df90
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-11-04 13:46:29 -0300

    header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { GlobalStyle } from './styles/global';
-
+import {Header} from './components/Header'
 function App() {
   return (
-    <div>
-      haha
+    <>
+      <Header/>
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
 
